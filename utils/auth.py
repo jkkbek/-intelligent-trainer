196ff6911ab630d888c89a569f9ca4da2fc7dd27
jkkbek <jksbk.aibek@gmail.com>
2026-04-25 01:44:13 +0500

    Fix password hashing with bcrypt_sha256

--- utils/auth.py
+++ utils/auth.py
@@ -1,6 +1,9 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt_sha256"],
+    deprecated="auto"
+)
 
 
 def hash_password(password: str) -> str:
